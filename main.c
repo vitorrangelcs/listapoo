a4920059042d3bc2470f4a256fc688ae56460c4f
vitorrangelcs <31075441+vitorrangelcs@users.noreply.github.com>
2017-08-17 01:33:08 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     int valor;
+     int m[12];
+     int teste;
+     char t;
+     int i;
+printf ("Digite um valor de 1 a 12: ");
+  scanf("%d", &valor);
+switch ( valor )
+  {
+     case 1 :
+       printf ("Janeiro\n");
+     break;
+
+     case 2 :
+       printf ("Fevereiro\n");
+     break;
+
+     case 3 :
+        printf ("Marco\n");
+     break;
+
+     case 4 :
+        printf ("Abril\n");
+     break;
+
+     case 5 :
+        printf ("Maio\n");
+     break;
+
+     case 6 :
+        printf ("Junho\n");
+     break;
+
+     case 7 :
+        printf ("Julho\n");
+
+     case 8 :
+        printf ("Agosto\n");
+     break;
+
+     case 9 :
+        printf ("Setembro\n");
+     break;
+
+     case 10 :
+        printf ("Outubro\n");
+     break;
+
+     case 11 :
+        printf ("Novembro\n");
+     break;
+
+     case 12 :
+        printf ("Dezembro\n");
+
+     break;
+
+
+     default :
+       printf ("Valor invalido!\n");
+  }
+    for (i =0; i <= 11; i++)
+    {
+        printf("informe o mes:\n");
+        scanf("%d",&m[i]);
+        if(m[i]==1){
+            printf ("Janeiro\n");
+        }if(m[i]==2){
+            printf ("Fevereiro\n");
+        }if(m[i]==3){
+            printf ("Marco\n");
+        }if(m[i]==4){
+            printf ("Abril\n");
+        }if(m[i]==5){
+            printf ("Maio\n");
+        }if(m[i]==6){
+            printf ("Junho\n");
+        }if(m[i]==7){
+            printf ("Julho\n");
+        }if(m[i]==8){
+            printf ("Agosto\n");
+        }if(m[i]==9){
+            printf ("Setembro\n");
+        }if(m[i]==10){
+            printf ("Outubro\n");
+        }if(m[i]==11){
+            printf ("Novembro\n");
+        }if(m[i]==12){
+            printf ("Dezembro\n");
+        }
+    }
+
+    return 0;
+}
+
